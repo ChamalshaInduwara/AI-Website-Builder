0a149e64f955d4df116b73b339e347b584710518
Chamalsha Induwara <chamalsha.20240765@iit.ac.lk>
2026-08-03 23:47:37 +0530

    add Outlet component to Layout for nested routing

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
+    <>
     <div>Layout</div>
+    <Outlet />
+    </>
+    
   );
 }
 
